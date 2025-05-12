92d7663edfc9a4787dcedf57fe7d76cd62d797d5
David Brochart <david.brochart@gmail.com>
2025-05-12 14:38:14 +0200

    Run tests on multiple Python versions (#129)
    * Run tests on multiple Python versions
    
    * Fix typing
    
    * Again

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from anyio import Lock, connect_tcp, create_memory_object_stream
 
 from pycrdt import Array, Doc
@@ -62,7 +64,7 @@ class Websocket:
 
 
 class ClientWebsocket:
-    def __init__(self, server_websocket: "ServerWebsocket"):
+    def __init__(self, server_websocket: ServerWebsocket):
         self.server_websocket = server_websocket
         self.send_stream, self.receive_stream = create_memory_object_stream[bytes](65536)
 
